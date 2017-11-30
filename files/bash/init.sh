7f73ffdad0eeeab88a877b8a4fa02a997f164a3a
Sander Zijlstra <sander.zijlstra@tmg.nl>
2017-11-30 15:42:00 +0100

    volledige path nodig natuurlijk

--- files/bash/init.sh
+++ files/bash/init.sh
@@ -4,9 +4,7 @@
 DOTFILES="base prompt aliases other"
 
 for dotfile in ${DOTFILES} ; do
-    if [ -f ${dotfile} ]; then
-        source $HOME/.bash/${dotfile}.sh
+    if [ -f ${HOME}/.bash/${dotfile} ]; then
+        source ${HOME}/.bash/${dotfile}.sh
     fi
 done
-
-#
