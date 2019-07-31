9030db54575a9a46d69391430448522c3e48fe5f
Sander Zijlstra <sander@zijlstra.it>
2019-07-31 13:23:56 +0200

    proper aliases

--- files/bash/aliases.sh
+++ files/bash/aliases.sh
@@ -7,7 +7,8 @@ alias ls='ls -G'
 alias ll='ls -G -l'
 alias la='ls -G -la'
 alias lt='ls -G -ltr'
-alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+alias sublime='open -a Sublime\ Text'
+alias typora='open -a Typora'
 
 if [ -f ~/bin/ssh-or-copy-id.sh ]; then
   alias ssh='~/bin/ssh-or-copy-id.sh'
