62f3a27d16812dccd8681c76699108bf7708fe60
Sander Zijlstra <sander@zijlstra.it>
2020-01-04 19:15:04 +0100

    bash completion added

--- files/bash/base.sh
+++ files/bash/base.sh
@@ -9,10 +9,12 @@ export HISTSIZE=10000
 # Try to load homebrew's config.
 # Then try to load Linux config.
 # Load the vendored script otherwise.
-if [ -f /usr/local/etc/bash_completion ]; then
-  . /usr/local/etc/bash_completion
+if [ -d $(brew --prefix)/etc/bash_completion.d ]; then
+	for file in `ls $(brew --prefix)/etc/bash_completion.d` ; do
+		. $(brew --prefix)/etc/bash_completion.d/${file}
+	done
 elif [ -f /etc/bash_completion ]; then
-  . /etc/bash_completion
+	. /etc/bash_completion
 fi
 
 # Extend PATH with personal bin dir.
