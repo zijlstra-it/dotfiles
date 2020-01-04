37b72f610754f1c9b8d3da5573588679370ee29c
Sander Zijlstra <sander@zijlstra.it>
2020-01-04 19:16:07 +0100

    python path for v3 and motd added

--- files/bash/other.sh
+++ files/bash/other.sh
@@ -1,2 +1,10 @@
 # ~/git/dotfiles/bash/other.sh
 #
+
+# Python
+unset PYTHONPATH
+export PATH=/usr/local/opt/python/libexec/bin:$PATH
+export PATH=$HOME/Library/Python/3.7/bin:$PATH
+
+# MOTD
+if [ -f $HOME/bin/motd ]; then $HOME/bin/motd; fi
